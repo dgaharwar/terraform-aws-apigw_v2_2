42201056bfd262f0089eb24d9ad3bf3da6d20cd6
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-11-12 19:41:34 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -19,25 +19,25 @@ output "apigatewayv2_api_execution_arn" {
 }
 
 # default stage
-output "default_apigatewayv2_stage_id" {
-  description = "The default stage identifier"
-  value       = try(aws_apigatewayv2_stage.default[0].id, "")
-}
+#output "default_apigatewayv2_stage_id" {
+#  description = "The default stage identifier"
+#  value       = try(aws_apigatewayv2_stage.default[0].id, "")
+#}
 
-output "default_apigatewayv2_stage_arn" {
-  description = "The default stage ARN"
-  value       = try(aws_apigatewayv2_stage.default[0].arn, "")
-}
+#output "default_apigatewayv2_stage_arn" {
+#  description = "The default stage ARN"
+#  value       = try(aws_apigatewayv2_stage.default[0].arn, "")
+#}
 
-output "default_apigatewayv2_stage_execution_arn" {
-  description = "The ARN prefix to be used in an aws_lambda_permission's source_arn attribute or in an aws_iam_policy to authorize access to the @connections API."
-  value       = try(aws_apigatewayv2_stage.default[0].execution_arn, "")
-}
+#output "default_apigatewayv2_stage_execution_arn" {
+#  description = "The ARN prefix to be used in an aws_lambda_permission's source_arn attribute or in an aws_iam_policy to authorize access to the @connections API."
+#  value       = try(aws_apigatewayv2_stage.default[0].execution_arn, "")
+#}
 
-output "default_apigatewayv2_stage_invoke_url" {
-  description = "The URL to invoke the API pointing to the stage"
-  value       = try(aws_apigatewayv2_stage.default[0].invoke_url, "")
-}
+#output "default_apigatewayv2_stage_invoke_url" {
+#  description = "The URL to invoke the API pointing to the stage"
+#  value       = try(aws_apigatewayv2_stage.default[0].invoke_url, "")
+#}
 
 output "default_apigatewayv2_stage_domain_name" {
   description = "Domain name of the stage (useful for CloudFront distribution)"
