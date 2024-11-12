26a55c31beae6fb33068a7891087fd00cd8741a1
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-11-12 14:55:51 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "region" {}
+variable "access_key" {}
+variable "secret_key {}
+
 variable "create" {
   description = "Controls if API Gateway resources should be created"
   type        = bool
